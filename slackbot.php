dad1d7fba134aba12c39616447037f10cb0dd535
0x4d/Martin Loretz <0x4d.dev@gmail.com>
2021-01-27 17:49:03 +0100

    Convert Slackbot to Plugin

--- slackbot.php
+++ slackbot.php
@@ -1,24 +1,26 @@
-// Insert into functions.php of wordpress theme
-// $webhookUrl's are removed, because they contain the slack keys, to get them create a slack integration
+<?php
+/**
+*Plugin Name: Slackbot
+*Description: Sends slack messages to the digitaleinitiativen slack channel
+**/
+
+// 1. Upload file to /wp-content/plugins/slackbot/slackbot.php
+// 2. Insert PRODUCTION_CHANNEL_URL and TEST_CHANNEL_URL (get them from the Slack App Directory)
+// 3. Activate plugin slackbot
+
 add_action('transition_post_status', 'transitionPostFunction', 10, 3 );
 function transitionPostFunction( $new_status, $old_status, $post ) {
     if ( $new_status == 'publish' && $old_status != 'publish' ) {
-		if (get_post_type($post->ID) === 'post') {
-        	sendSlack($post->ID);
-		}
+        sendSlack($post->ID);
     }
 }
 
-/*add_action('publish_post', 'publishPostFunction',10,2); // publish_post
-function publishPostFunction($post_id, $post){
-    if ( !(defined( 'REST_REQUEST' ) && REST_REQUEST )) {
-	// https://wordpress.stackexchange.com/a/120999
-		sendSlack($post->ID);
-	}
-}*/
-
 function sendSlack($post_id){	
+	if(!(get_post_type($post_id) == 'post' || get_post_type($post_id) == 'page')) return;
+	
+	// send slack messages to the #bots-testfield channel instead of general
 	$testChannel = false;
+	
 	$webhookUrl = !$testChannel ? 'PRODUCTION_CHANNEL_URL' : 'TEST_CHANNEL_URL';
 		
 	$content_post = get_post($post_id);
@@ -50,16 +52,26 @@ function sendSlack($post_id){
 	$data['attachments'] = array();
 	$data['attachments'][0] = $attachments;
 
-		$json_data = json_encode($data);
-		// Initiate the cURL
-		$url = curl_init($webhookUrl);
-		curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($url, CURLOPT_POSTFIELDS, $json_data);
-		curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($url, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			'Content-Length: ' . strlen($json_data))
-				   );
-		// The results of our request, to use later if we want.
-		$result = curl_exec($url);
+	$json_data = json_encode($data);
+	$url = curl_init($webhookUrl);
+	curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($url, CURLOPT_POSTFIELDS, $json_data);
+	curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($url, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Content-Length: ' . strlen($json_data))
+	);
+	$result = curl_exec($url);
 }
+
+/*
+add_action('publish_post', 'publishPostFunction',10,2); // publish_post
+function publishPostFunction($post_id, $post){
+    if ( !(defined( 'REST_REQUEST' ) && REST_REQUEST )) {
+		// https://wordpress.stackexchange.com/a/120999
+		sendSlack($post->ID);
+	}
+}
+*/
+
+?>
